a7e48601e8c4d11f827711ad65e637dfe84aa2e1
Siddharth Sharma <hearsid@gmail.com>
2022-12-09 23:56:21 +0000

    no errors on npm run dev

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,61 +1,67 @@
 <script lang="ts">
   import type { IContact } from "$lib/contact.type";
   import { page } from '$app/stores';
+  import Contact from './Contact.svelte';
 
   let title = 'Contacts';
-  let contacts: IContact[];
+  let contacts: IContact[] = [];
   let selectedContact: IContact;
   let total_contacts: number;
 
   const no_of_contacts = $page.url.searchParams.get('no_of_contacts');
 
 
-        total_contacts = +param['']; // toInt
-        total_contacts = Number(total_contacts);
-        contacts = this.contactService.generateContacts(this.total_contacts);
-        this.getContacts(this.total_contacts);
+        // total_contacts = +param['']; // toInt
+        // total_contacts = Number(total_contacts);
+        // contacts = this.contactService.generateContacts(this.total_contacts);
+        // this.getContacts(this.total_contacts);
 
 
   function getContacts(total: number): void {
     //  this.contactService.getContacts().then(contacts => { this.contacts = contacts });
     const contactCount = total || 10;
-    this.contactService.getContactList(contactCount)
-      .subscribe(
-        (contacts: IContact[]) => {
-          this.contactService.contacts = contacts;
-          this.contacts = contacts;
-        },
-        (error: any) => console.error('Error: ' + error),
-        () => console.log('Completed!')
-      );
+    // this.contactService.getContactList(contactCount)
+    //   .subscribe(
+    //     (contacts: IContact[]) => {
+    //       this.contactService.contacts = contacts;
+    //       this.contacts = contacts;
+    //     },
+    //     (error: any) => console.error('Error: ' + error),
+    //     () => console.log('Completed!')
+    //   );
   }
 
   function onSelect(contact: IContact): void {
-    this.selectedContact = contact;
+    // this.selectedContact = contact;
   }
 
-  function delete(index: number): void {
-    this.contactService.deleteContact(index);
+  function deleteContact(event: any): void {
+    const index: number = event.detail.text;
+    // this.contactService.deleteContact(index);
     //    this.getContacts();
   }
 
   function addContact(): void {
     // set active component to null to determine edit/add
-    this.contactService.activeContact = new IContact();
-    this.router.navigate(['/newContact']);
+    // this.contactService.activeContact = new IContact();
+    // this.router.navigate(['/newContact']);
   }
 </script>
 
 <h2 class="page-header text-center">
-  {title} ({contacts?.length?.toLocaleString()})</h2>
+	{title} ({contacts?.length?.toLocaleString()})
+</h2>
 <!-- <p class="text-center">
     <a (click)="addContact()" class="btn btn-lg btn-outline" >Add Contact</a>
 </p> -->
 <div class="media-list contacts-container">
-    <ng-container *ngFor="let contact of contacts;let i = index">
-        <contact [contact]="contact" 
-            [index]="i" 
-            class="my-repeat-animation contact media"
-            (delete)="delete($event)"></contact>
-    </ng-container>
+  {#each contacts as contact, i}
+      <Contact
+      contact="contact"
+      index="i"
+      class="my-repeat-animation contact media"
+      on:remove={deleteContact}
+      ()="delete($event)"
+    />
+  {/each}
 </div>
--- src/routes/contacts/Contact.svelte
+++ src/routes/contacts/Contact.svelte
@@ -1,13 +1,32 @@
-<div class="thumbnail"> 
-    <img class="media-object contact-image" src="assets/img/faces/{{contact.id || contact.imageId}}.jpg">
-</div>
+<script lang="ts">
+	export let contact: any;
+	export let index: any;
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
 
-<div class="media-body">
-    <div class="media-heading">
-        <h3>
-            {{index + 1}})
-            {{contact.name}}
-            <!-- <small>
+	function removeContact(i: any) {
+        dispatch('remove', {
+			index: i
+		});
+    }
+
+</script>
+
+<div class={$$props.class}>
+	<div class="thumbnail">
+		<img
+			class="media-object contact-image"
+			src="assets/img/faces/{contact.id || contact.imageId}.jpg"
+            alt="contact image"
+		/>
+	</div>
+
+	<div class="media-body">
+		<div class="media-heading">
+			<h3>
+				{index + 1})
+				{contact.name}
+				<!-- <small>
                 <a (click)="edit()">
                     <i class="glyphicon glyphicon-pencil"></i>
                 </a>
@@ -15,20 +34,25 @@
                     <i class="gg-trash"></i>
                 </a>
             </small> -->
-            <a (click)="removeContact(index)">
-                <i class="gg-trash"></i>
-            </a>
-        </h3>
-    </div>
+				<a
+					on:click={() => {
+						removeContact(index);
+					}}
+				>
+					<i class="gg-trash" />
+				</a>
+			</h3>
+		</div>
 
-    <div class="info-container">
-        <div class="row">
-            <div class="label">Phone Number:</div>
-            <div class="value">{{contact.tel}}</div>
-        </div>
-        <div class="row">
-            <div class="label">Email:</div>
-            <div class="value">{{contact.email}}</div>
-        </div>
-    </div>
+		<div class="info-container">
+			<div class="row">
+				<div class="label">Phone Number:</div>
+				<div class="value">{contact.tel}</div>
+			</div>
+			<div class="row">
+				<div class="label">Email:</div>
+				<div class="value">{contact.email}</div>
+			</div>
+		</div>
+	</div>
 </div>
